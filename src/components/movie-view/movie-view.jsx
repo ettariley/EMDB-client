2bf0f97ef98cc69c24d14b6ad01de15d7641fc39
Etta Baker <etta.r.baker@gmail.com>
2022-04-24 00:21:34 -0400

    Fixed Genre & Director Name errors

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export class MovieView extends React.Component {
+  
   render() {
     const { movie, onBackClick } = this.props;
     return (
@@ -18,11 +19,11 @@ export class MovieView extends React.Component {
         </div>
         <div className='movie-genre'>
           <span className='label'>Genre: </span>
-          <span className='value'>{movie.Genre}</span>
+          <span className='value'>{movie.Genre.Name}</span>
         </div>
         <div className='movie-director'>
           <span className='label'>Director: </span>
-          <span className='value'>{movie.Director}</span>
+          <span className='value'>{movie.Director.Name}</span>
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
