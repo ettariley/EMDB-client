fcb2764e869a34ad7cd523f2a2d6507221b27b3f
Etta Baker <etta.r.baker@gmail.com>
2022-05-21 01:19:03 -0400

    Updated stylings

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import { Form } from "react-bootstrap";
 import { setFilter } from "../../actions/actions";
+import './visibility-filter-input.scss';
 
 function VisibilityFilterInput(props) {
   return <Form.Control 
+    className="filter-form"
     onChange={e => props.setFilter(e.target.value)}
     value={props.visibilityFilter}
-    placeholder="filter"
+    placeholder="Search Movie Titles"
   />;
 }
 
