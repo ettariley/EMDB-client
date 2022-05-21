ec74196631cf9fbdafb9d5aba23f6c0ff39decdf
Etta Baker <etta.r.baker@gmail.com>
2022-05-21 01:15:35 -0400

    Added reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { SET_FILTER, SET_MOVIES } from "../actions/actions";
+import { SET_FILTER, SET_MOVIES, SET_USER } from "../actions/actions";
 
 function visibilityFilter(state = '', action) {
   switch (action.type) {
@@ -19,10 +19,20 @@ function movies(state = [], action) {
   }
 }
 
+function user(state = '', action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
 function moviesApp(state = {}, action) {
   return {
     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-    movies: movies(state.movies, action)
+    movies: movies(state.movies, action),
+    user: user(state.user, action),
   }
 }
 
