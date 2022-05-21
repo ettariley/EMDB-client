c7172faa20464f0e20b82b74ecae7502527a715c
Etta Baker <etta.r.baker@gmail.com>
2022-05-21 01:17:48 -0400

    Updated with Redux

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 import { connect } from "react-redux";
-import { MovieCard } from '../movie-card/movie-card';
+import MovieCard from '../movie-card/movie-card';
 import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
 
 const mapStateToProps = state => {
@@ -24,7 +24,7 @@ function MoviesList(props) {
       <VisibilityFilterInput visibilityFilter={visibilityFilter} />
     </Col>
     {filteredMovies.map(m => (
-      <Col md={4} key={m._id}>
+      <Col className="movie-list" md={4} key={m._id}>
         <MovieCard movie={m} />
       </Col>
     ))}
